8a48f4c4a803317292bf89b29b4612f4ec9def8f
An LP Nguyen <nguyenan169@gmail.com>
2016-09-09 08:48:50 +0700

    Initial Commit

---
+++ CoreMediaIO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreMediaIO
+//
+//  Created by An Le Phu Nguyen on 9/9/16.
+//  Copyright © 2016 An Le Phu Nguyen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ CoreMediaIO/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  CoreMediaIO
+//
+//  Created by An Le Phu Nguyen on 9/9/16.
+//  Copyright © 2016 An Le Phu Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
